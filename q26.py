27c14b69fe011a3b8aab07f77bd34f6edb00b5e9
engrsalman17-gif <engr.salman17@gmail.com>
2026-02-27 00:21:53 +0000

    done

---
+++ q26.py
@@ -0,0 +1,4 @@
+x = int(input("please enter the number of minutes you want to convert into hours : "))
+hours = str(x // 60)
+minutes = str(x % 60)
+print ("Hours = " + hours + " minutes = " + minutes)
