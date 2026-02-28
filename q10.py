327c86dc4ebe92f527ac0b6c19744a5e40a91df5
engrsalman17-gif <engr.salman17@gmail.com>
2026-02-28 00:05:38 +0000

    done

---
+++ q10.py
@@ -0,0 +1,6 @@
+# Experiment: What happens if you try to add a string and an integer in a print statement?
+# Write a code snippet that fixes this using str()
+y = 2
+#print ("Pakistan"+y)
+y = str(2)
+print ("pakistan"+y)
