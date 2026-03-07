f0b38442b95f376d15a33456dece880ab5e76c0f
engrsalman17-gif <engr.salman17@gmail.com>
2026-03-07 05:37:45 +0000

    done

--- python-list-practice.py
+++ python-list-practice.py
@@ -4,3 +4,9 @@ list1 = ["salman", "sindh", "pakistan"]
 print(list1[0])
 print(list1[1])
 print(list1[2])
+
+customer_list = ["Nexskill", 10, 2.5, True]
+print(customer_list[0])
+print(customer_list[1])
+print(customer_list[2])
+print(customer_list[3])
